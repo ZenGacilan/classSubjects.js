af663176733309490eaa5ececf41ba30f7a8c5b2
ZenGacilan <potapetekau@gmail.com>
2024-11-04 18:53:37 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,9 @@
+// Collect user information
+const subjectTitle = prompt("Data Structures And Algorithms:");
+const classSchedule = prompt("Monday and Friday (8:00 - 9:30, monday and friday):");
+const classroom = prompt("Comlab B:");
+const classInstructor = prompt("Vitruvius John Barayuga:");
+const studentName = prompt("Gian Carlo:");
+
+// Log the information to the console in the specified format
+console.log(`{Gian Carlo Gacilan} is currently enrolled in {Data Structures And Algorithms} with a class schedule of {Monday And Friday} at room {Comlab B}. The instructor for the subject is {Vitruvius John Barayuga}`);
